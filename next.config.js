330bedad3cf5b6ce1148ded1fb0773629c0e17d1
ning <xkfkr13@gmail.com>
2026-02-13 20:42:28 +0900

    build/vercel
    next.config.js 수정

--- next.config.js
+++ next.config.js
@@ -4,6 +4,13 @@ const nextConfig = {
   eslint: {
     ignoreDuringBuilds: true,
   },
+  async rewrites() {
+    return [
+      {
+        source: '/api/:path*',
+        destination: 'http://soundock-alb-se1-75264038.ap-northeast-2.elb.amazonaws.com/api/:path*',
+      },
+    ];
+  },
 };
-
 module.exports = nextConfig;
