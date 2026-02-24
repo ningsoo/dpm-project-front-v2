88ccc3be2b6bc5da7804d8a5537225a28ab67261
김성준 <prmorek@gmail.com>
2026-02-24 12:09:09 +0900

    docs : OWASP ZAP 점검 취약점 수정
    - SCP 헤더 추가

--- next.config.js
+++ next.config.js
@@ -1,9 +1,29 @@
 /** @type {import('next').NextConfig} */
+const csp = `
+default-src 'self';
+base-uri 'self';
+form-action 'self';
+object-src 'none';
+frame-ancestors 'self';
+
+script-src 'self' https://accounts.google.com https://apis.google.com;
+
+style-src 'self' 'unsafe-inline' https://fonts.googleapis.com;
+font-src 'self' https://fonts.gstatic.com data:;
+
+img-src 'self' data: https: https://soundock-dev-uploads.s3.amazonaws.com;
+
+connect-src 'self' https://www.soundock.live;
+
+frame-src https://accounts.google.com;
+`;
+
 const nextConfig = {
   reactStrictMode: true,
   eslint: {
     ignoreDuringBuilds: true,
   },
+
   async rewrites() {
     const backendUrl = process.env.BACKEND_URL || 'http://localhost:8080';
     return [
@@ -21,5 +41,20 @@ const nextConfig = {
       },
     ];
   },
+
+  async headers() {
+    return [
+      {
+        source: '/(.*)',
+        headers: [
+          {
+            key: 'Content-Security-Policy',
+            value: csp.replace(/\s{2,}/g, ' ').trim(),
+          },
+        ],
+      },
+    ];
+  },
 };
+
 module.exports = nextConfig;
